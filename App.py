a753e258c6ebbcf709f57896ea205179bfab8c2a
juan jose <l21590492@sjuanrio.tecnm.mx>
2025-09-24 20:07:44 -0600

    Primer commit: Estructura base del proyecto y archivos iniciales

---
+++ App.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+#crear instancia
+app =  Flask(__name__)
+
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://juan:eDjBmVrmq5lzPg6nSBDVMWB7Xib62gm3@dpg-d2vp4fali9vc7388rchg-a.oregon-postgres.render.com/db_tec_qq1p'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+#Modelo de la base de datos
+class Estudiante(db.Model):
+    __tablename__ = 'estudiantes'
+    no_control = db.Column(db.String, primary_key=True)
+    nombre = db.Column(db.String)
+    ap_paterno = db.Column(db.String)
+    ap_materno = db.Column(db.String)
+    semestre = db.Column(db.Integer)
+
+    def to_dict(self):
+        return{
+            'no_control': self.no_control,
+            'nombre': self.nombre,
+            'ap_paterno': self.ap_paterno,
+            'ap_materno': self.ap_materno,
+            'semestre': self.semestre,
+        }
+
+
+#Ruta raiz
+@app.route('/')
+def index():
+    #retornar los alumnos
+    #return 'Hola Mundo'
+
+    #Trae todos los estudiantes
+    estudiantes = Estudiante.query.all()
+    return render_template('index.html', estudiantes = estudiantes)
+
+#Ruta /alumnos crear un nuevo alumno
+@app.route('/estudiantes/new', methods=['GET','POST'])
+def create_estudiante():
+    if request.method == 'POST':
+        #Agregar Estudiante
+        no_control = request.form['no_control']
+        nombre = request.form['nombre']
+        ap_paterno = request.form['ap_paterno']
+        ap_materno = request.form['ap_materno']
+        semestre = request.form['semestre']
+
+        nvo_estudiante = Estudiante(no_control=no_control, nombre=nombre, ap_paterno=ap_paterno, ap_materno= ap_materno, semestre= semestre)
+
+        db.session.add(nvo_estudiante)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    
+    #Aqui sigue si es GET
+    return render_template('create_estudiante.html')
+
+
+#Eliminar estudiante
+@app.route('/estudiantes/delete/<string:no_control>')
+def delete_estudiante(no_control):
+    estudiante = Estudiante.query.get(no_control)
+    if estudiante:
+        db.session.delete(estudiante)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+#Actualizar alumno
+@app.route('/alumnos/update/<string:no_control>', methods=['GET','POST'])
+def update_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if request.method == 'POST':
+        alumno.nombre = request.form['nombre']
+        alumno.ap_paterno = request.form['ap_paterno']
+        alumno.ap_materno = request.form['ap_materno']
+        alumno.semestre = request.form['semestre']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_alumno.html', alumno=alumno)
+
+
+
+#Ruta /alumnos
+@app.route('/alumnos')
+def getAlumnos():
+    return 'Aqui van los alumnos'
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
